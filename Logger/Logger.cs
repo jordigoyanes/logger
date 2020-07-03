d7f51d7ffe0730b7f4fd5cf18acefbc815cb2678
Jordi Goyanes Granados <jordigoyanes@gmail.com>
2020-07-03 16:12:54 +0200

    Add project files

---
+++ Logger/Logger.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Logger
+{
+    public enum LogTarget
+    {
+        File, Console, EventLog
+    }
+
+    public abstract class LogBase
+    {
+        protected readonly object lockObj = new object();
+        public abstract void Log(string message);
+    }
+
+    public class FileLogger : LogBase
+    {
+        public string filePath = string.Empty;
+        public FileLogger(string filepath)
+        {
+            filePath = @filepath;
+        }
+        public override void Log(string message)
+        {
+            lock (lockObj)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                    streamWriter.Close();
+                }
+            }
+        }
+    }
+
+    public class EventLogger : LogBase
+    {
+        public override void Log(string message)
+        {
+            lock (lockObj)
+            {
+                EventLog m_EventLog = new EventLog("");
+                m_EventLog.Source = "IDGEventLog";
+                m_EventLog.WriteEntry(message);
+            }
+        }
+    }
+
+    public class ConsoleLogger : LogBase
+    {
+        public override void Log(string message)
+        {
+            lock (lockObj)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+    public static class LogHelper
+    {
+        private static LogBase logger = null;
+        public static void Log(LogTarget target, string message)
+        {
+            switch (target)
+            {
+                case LogTarget.File:
+                    logger = new FileLogger("Logger.txt");
+                    logger.Log(message);
+                    break;
+                case LogTarget.Console:
+                    logger = new ConsoleLogger();
+                    logger.Log(message);
+                    break;
+                case LogTarget.EventLog:
+                    logger = new EventLogger();
+                    logger.Log(message);
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+
+}
---
+++ Logger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //test code
+            LogHelper.Log(LogTarget.File, "text to file");
+
+            FileLogger customPath = new FileLogger("customfile.txt");
+            customPath.Log("test");
+
+            LogHelper.Log(LogTarget.Console, "console test");
+
+            Console.ReadLine();
+        }
+    }
+}
